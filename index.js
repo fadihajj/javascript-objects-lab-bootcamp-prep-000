a33828db8fc54ec9b257eff1f9532daa53cdd547
Fadi hajj <fadi.elhajj@lau.edu>
2018-08-11 23:00:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ var recipes = {
 
 function updateObjectWithKeyAndValue(obj,key,val){
 var obj1 = Object.assign({},obj);
-obj1[key] = val;return obj1;
+obj1[key] = val;
+return obj1;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj,key,val){
   obj[key] = val;
   return obj;
+}
+
+function deleteFromObjectByKey(obj,key){
+  delete obj1[key];
+  return obj1;
 }
